25de8449104c47abcd8627118951288ad581f0c7
mimvoid <mimvoid@proton.me>
2024-05-28 16:22:29 -0400

    fixed fuzzel colors

--- home-manager/hyprland/fuzzel.nix
+++ home-manager/hyprland/fuzzel.nix
@@ -1,34 +1,35 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   programs.fuzzel = {
     enable = true;
     settings = {
       main = {
-        dip-aware = true;
+        dpi-aware = true;
         font = "SauceCodePro Nerd Font:size=9";
         #terminal = "foot";
-        width = 74;
+        width = 72;
         lines = 36;
         layer = "overlay";
-        prompt = "> ";
+        prompt = ">  ";
         #icon-theme = "Rowaita-Pink-Dark";
         exit-on-keyboard-focus-loss = false;
 
         horizontal-pad = 32;
         vertical-pad = 18;
         inner-pad = 16;
+        border-radius = 4;
 
         fields = "filename,name";
       };
       colors = {
         background = "24273a" + "ee"; # base
-        text = "cad365" + "ff"; # text
+        text = "cad3f5" + "ff"; # text
         match = "f0bdef" + "ff";
-        selection = "494d64" + "dd";
-        selection-match = "f5bde6" + "ff";
-        selection-text = "f4dbd6" + "ff";
-        border = "ee99a0" + "ff"; 
+        selection = "494d64" + "dd"; #surface1
+        selection-match = "f5bde6" + "ff"; # pink
+        selection-text = "f4dbd6" + "ff"; # rosewater
+        border = "ee99a0" + "ff"; # maroon
       };
     };
   };
