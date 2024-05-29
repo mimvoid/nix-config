a38ca3ab42fd9166d2f925c53cd9d33304dc8476
mimvoid <mimvoid@proton.me>
2024-05-29 01:22:11 -0400

    made waybar more colorful

--- home-manager/hyprland/hyprland.nix
+++ home-manager/hyprland/hyprland.nix
@@ -181,7 +181,7 @@ in
                 "$mod, Q, killactive"
 
                 # Reload
-                "$mod, R, exec, pkill waybar && hyprctl reload config-only"
+                "$mod, R, exec, pkill waybar ; hyprctl reload config-only"
 
                 # Trigger wlogout
                 "$mod SHIFT, Q, exec, wlogout -b 2"
