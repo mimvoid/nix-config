d366d6e04cd26bb226ac3a8f1b0df505717b553e
mimvoid <mimvoid@proton.me>
2024-05-30 02:09:38 -0400

    updated foot settings

--- home-manager/terminal/foot.nix
+++ home-manager/terminal/foot.nix
@@ -1,5 +1,24 @@
-{ config, pkgs, inputs, ...}:
+{ ... }:
+
+# Colors and fonts are managed by Stylix
 
 {
-  programs.foot.enable = true;
+    programs.foot = {
+        enable = true;
+        settings = {
+            main.pad = "12x0";
+            scrollback = {
+                lines = 1000;
+                indicator-position = "fixed";
+                indicator-format = "percentage";
+            };
+            cursor = {
+                style = "block";
+                unfocused-style = "hollow";
+            };
+            mouse = {
+                hide-when-typing = "yes";
+            };
+        };
+    };
 }
