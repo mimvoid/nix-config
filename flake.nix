fec29f9b14b0bcc84ca3dbdb1a5f6a973abcf042
mimvoid <mimvoid@proton.me>
2024-06-05 23:33:44 -0400

    added nixpkgs-unstable to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,57 +1,78 @@
- {
-    description = "NixOS & Home Manager flake configuration";
+{
+  description = "NixOS & Home Manager flake configuration";
 
-    inputs = {
-        # Nix packages
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-        # Declarative flatpaks
-        #nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.4.1";
+    # Declarative flatpaks
+    #nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.4.1";
 
-        # Home Manager & modules
-        home-manager = {
-            url = "github:nix-community/home-manager/release-24.05";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        nixvim = {
-            url = "github:nix-community/nixvim";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        nix-colors.url = "github:misterio77/nix-colors";
-        stylix.url = "github:danth/stylix/master";
-        plasma-manager = {
-            url = "github:pjones/plasma-manager/trunk";
-            inputs.nixpkgs.follows = "nixpkgs";
-            inputs.home-manager.follows = "home-manager";
-        };
+    # Home Manager
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    outputs = inputs @ { self, nixpkgs, home-manager, nixvim, nix-colors, stylix, plasma-manager, ... }:
-    let
-        allowed-unfree-packages = [
-            "zoom"
+    # Extra modules
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-colors.url = "github:misterio77/nix-colors";
+    stylix.url = "github:danth/stylix/master";
+    plasma-manager = {
+      url = "github:pjones/plasma-manager/trunk";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-unstable,
+    home-manager,
+    nixvim,
+    nix-colors,
+    stylix,
+    plasma-manager,
+    ... } @ inputs:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      
+      # Allows nixpkgs-unstable to be referenced with pkgs.unstable.<package>
+      overlays = [
+        (self: super: {
+          unstable = nixpkgs-unstable.legacyPackages.${self.system};
+        })
+      ];
+    };
+    allowed-unfree-packages = ["zoom"];
+  in
+  {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs allowed-unfree-packages; };
+        modules = [ ./configuration.nix ];
+      };
+    };
+    
+    homeConfigurations = {
+      "zinnia" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = { inherit inputs allowed-unfree-packages; };
+        modules = [
+          ./home-manager/home.nix
+          #nix-flatpak.homeManagerModules.nix-flatpak
+          stylix.homeManagerModules.stylix
+          nixvim.homeManagerModules.nixvim
+          plasma-manager.homeManagerModules.plasma-manager
         ];
-    in
-    {
-        nixosConfigurations = {
-            nixos = nixpkgs.lib.nixosSystem {
-                system = "x86_64-linux";
-                specialArgs = { inherit inputs allowed-unfree-packages; };
-                modules = [ ./configuration.nix ];
-            };
-        };
-        homeConfigurations = {
-            "zinnia" = home-manager.lib.homeManagerConfiguration {
-                pkgs = nixpkgs.legacyPackages.x86_64-linux;
-                extraSpecialArgs = { inherit inputs allowed-unfree-packages; };
-                modules = [
-                    ./home-manager/home.nix
-                    #nix-flatpak.homeManagerModules.nix-flatpak
-                    stylix.homeManagerModules.stylix
-                    nixvim.homeManagerModules.nixvim
-                    plasma-manager.homeManagerModules.plasma-manager
-                ];
-            };
-        };
+      };
     };
- }
+  };
+}
