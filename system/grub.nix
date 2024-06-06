96a0dfd2db2db9ab60f2eaefc5c1d688e8049189
mimvoid <mimvoid@proton.me>
2024-06-06 01:27:50 -0400

    disabled grub due to install issue

--- system/grub.nix
+++ system/grub.nix
@@ -3,23 +3,23 @@
 {
   boot.loader = {
     efi.canTouchEfiVariables = true;
-    systemd-boot.enable = false;
+    systemd-boot.enable = true;
   };
     
-  boot.loader.grub = {
-    enable = true;
-    device = ''${config.fileSystems."/boot".device}'';
-    efiSupport = true;
-    useOSProber = true;
+#  boot.loader.grub = {
+#    enable = true;
+#    device = ''${config.fileSystems."/boot".device}'';
+#    efiSupport = true;
+#    useOSProber = true;
 
-    # Default GRUB has such small text
-    # This makes it more readable for me
-    font = "${pkgs.fira-code}/share/fonts/truetype/FiraCode-VF.ttf";
-    fontSize = 16; # Only works with ttf or otf fonts
+#    # Default GRUB has such small text
+#    # This makes it more readable for me
+#    font = "${pkgs.fira-code}/share/fonts/truetype/FiraCode-VF.ttf";
+#    fontSize = 16; # Only works with ttf or otf fonts
 
     #splashImage = 
     #splashMode = "normal";
-    backgroundColor = "#1e1e2e"; # Goes behind the splash image
-    timeoutStyle = "menu";
-  };
+#    backgroundColor = "#1e1e2e"; # Goes behind the splash image
+#    timeoutStyle = "menu";
+#  };
 }
