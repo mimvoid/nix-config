2d23a3b942fbe5f813aced861e7c09f394a63fc0
mimvoid <mimvoid@proton.me>
2024-06-08 16:09:43 -0400

    attempt with xdg.configFile for Thunar

--- home-manager/guis/thunar.nix
+++ home-manager/guis/thunar.nix
@@ -9,15 +9,24 @@
     gvfs
   ];
 
-  xfconf.settings = {
-    thunar = {
-      "last-view" = "ThunarIconView";
-      "last-icon-view-zoom-level" = "THUNAR_ZOOM_LEVEL_100_PERCENT";
-      "last-separator-position" = 170;
-      "last-window-maximized" = true;
-      "last-window-width" = 640;
-      "last-window-height" = 480;
-      "last-show-hidden" = true;
+  # Unused for now
+  xdg.configFile = {
+    ".config/xfce4/xfconf/xfce-perchannel-xml/thunar.xml" = {
+      enable = false;
+      text = # xml
+      ''
+        <channel name="thunar" version="1.0">
+          <property name="last-view" type="string" value="ThunarIconView"/>
+          <property name="last-icon-view-zoom-level" type="string" value="THUNAR_ZOOM_LEVEL_100_PERCENT"/>
+          <property name="last-separator-position" type="int" value="170"/>
+          <property name="last-window-maximized" type="bool" value="true"/>
+          <property name="last-window-width" type="int" value="640"/>
+          <property name="last-window-height" type="int" value="480"/>
+          <property name="last-show-hidden" type="bool" value="true"/>
+          <property name="misc-single-click" type="bool" value="false"/>
+          <property name="misc-case-sensitive" type="bool" value="false"/>
+        </channel>
+      '';
     };
   };
 }
