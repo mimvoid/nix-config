f0eb560ffc5570ebb974a78d83673ff22135b3d6
mimvoid <mimvoid@proton.me>
2024-06-08 13:57:10 -0400

    updated treesitter settings

--- home-manager/nixvim/plugins/treesitter.nix
+++ home-manager/nixvim/plugins/treesitter.nix
@@ -1,7 +1,35 @@
 {
-	programs.nixvim.plugins.treesitter = {
-	  enable = true;
-	  indent = true;
-	  nixGrammars = true;
+	programs.nixvim.plugins = {
+    treesitter = {
+	    enable = true;
+	    indent = true;
+	    nixGrammars = true;
+      nixvimInjections = true;
+
+      ensureInstalled = [
+        "bash"
+        "comment"
+        "css"
+        "git_config"
+        "git_rebase"
+        "gitattributes"
+        "gitcommit"
+        "gitignore"
+        "html"
+        "hyprlang"
+        "ini"
+        "latex"
+        "lua"
+        "markdown"
+        "markdown_inline"
+        "nix"
+        "scss"
+        "yaml"
+        "yuck"
+      ];
+    };
+    treesitter-textobjects = {
+      enable = true;
+    };
   };
 }
