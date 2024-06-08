bfe605623153ee09063b3b973bc75650f85776f9
mimvoid <mimvoid@proton.me>
2024-06-08 14:56:20 -0400

    updated gui configs

--- home-manager/guis/default.nix
+++ home-manager/guis/default.nix
@@ -1,48 +1,45 @@
 { pkgs, lib, allowed-unfree-packages, ... }:
 
 {
-    home.packages = with pkgs; [
-        # Thunar
-        xfce.thunar
-        xfce.thunar-archive-plugin
-        xfce.thunar-media-tags-plugin
-        gvfs
+  home.packages = with pkgs; [
+    # Thunar
+    xfce.thunar
+    xfce.thunar-archive-plugin
+    xfce.thunar-media-tags-plugin
+    gvfs
 
-        # Art & design
-        #krita Seems to be broken right now
+    # Art & design
+    #krita Seems to be broken right now
 
-        # Documents
-        pdfarranger
-        zotero_7
-        zathura
+    # Documents
+    pdfarranger
+    zotero_7
 
-        # Contact
-        zoom-us
-        vesktop
+    # Contact
+    zoom-us
+    vesktop
 
-        # Misc
-        anki
-        bottles
-        freetube
-    ];
+    # Misc
+    anki
+    bottles
+  ];
 
-    imports = [
-        ./freetube.nix
-    ];
+  imports = [
+    ./freetube.nix
+    ./zathura.nix
+  ];
 
-    programs.zathura.enable = true;
+  # Imports list of allowed unfree packages from flake.nix
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) allowed-unfree-packages; 
 
-    # There seems to be a bug with flatpak
-    # that prevents nix-flatpak from working
-    # so this will be commented for now
+  # There seems to be a bug with flatpak
+  # that prevents nix-flatpak from working
+  # so this will be commented for now
 
-    #services.flatpak.packages = [ "" ];
+  #services.flatpak.packages = [ "" ];
 
-    #services.flatpak.update.auto = {
-    #    enable = true;
-    #    onCalendar = "weekly";
-    #};
-
-    # Imports list of allowed unfree packages from flake.nix
-    nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) allowed-unfree-packages; 
+  #services.flatpak.update.auto = {
+  #    enable = true;
+  #    onCalendar = "weekly";
+  #};
 }
---
+++ home-manager/guis/zathura.nix
@@ -0,0 +1,15 @@
+{
+  programs.zathura = {
+    enable = true;
+    options = {
+      adjust-open = "best-fit";
+      double-click-follow = true;
+      selection-notification = false;
+      show-hidden = true;
+      show-signature-information = true;
+      statusbar-home-tilde = true;
+      statusbar-page-percent = true;
+      window-title-home-tilde = true;
+    };
+  };
+}
