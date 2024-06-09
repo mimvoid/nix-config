db5febfa9348af514e025fc294c842595e6f9717
mimvoid <mimvoid@proton.me>
2024-06-08 21:05:15 -0400

    added obsidian desktop entry icon

--- home-manager/desktop.nix
+++ home-manager/desktop.nix
@@ -1,8 +1,10 @@
+{ pkgs, ... }:
 # Desktop entries
 {
   xdg.desktopEntries = {
     obsidian = {
       name = "Obsidian.AppImage";
+      icon = "${pkgs.papirus-icon-theme}/share/icons/Papirus/48x48/apps/obsidian.svg";
 
       # TODO: what is the variable for an appimageTool package?
       exec = "${../packages/result/bin/Obsidian} %u";
