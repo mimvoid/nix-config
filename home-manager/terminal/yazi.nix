753cbb8961628b864922dd0b6eec637104dccc75
mimvoid <mimvoid@proton.me>
2024-06-09 20:47:29 -0400

    updated yazi settings

--- home-manager/terminal/yazi.nix
+++ home-manager/terminal/yazi.nix
@@ -1,25 +1,32 @@
 { pkgs, ... }:
 
 {
-    home.packages = with pkgs; [
-        ffmpegthumbnailer
-        poppler
-        unar
-        wl-clipboard
+  home.packages = with pkgs; [
+    ffmpegthumbnailer
+    poppler
+    unar
+    wl-clipboard
+  ];
 
-        dooit
-        wttrbar
-        wego
-    ];
-
-    programs.yazi = {
-        enable = true;
-        package = pkgs.yazi;
-        enableBashIntegration = true;
-        settings.manager = {
-            show_hidden = true;
-            sort_by = "natural";
-            sort_dir_first = true;
-        };
+  programs.yazi = {
+    enable = true;
+    package = pkgs.yazi;
+    enableZshIntegration = true;
+    settings = {
+      manager = {
+        ratio = [1 4 3];
+        show_hidden = true;
+        show_symlink = true;
+        sort_by = "natural";
+        sort_sensitive = false;
+        sort_reverse = false;
+        sort_dir_first = true;
+        sort_translit = true;
+        linemode = "none";
+      };
+      preview = {
+        tab_size = 2;
+      };
     };
+  };
 }
