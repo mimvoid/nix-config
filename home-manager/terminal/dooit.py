9876c60dddb60adfef20f7b7ea262292a92de103
mimvoid <mimvoid@proton.me>
2024-06-14 16:22:12 -0400

    fixed dooit config

--- home-manager/terminal/dooit.py
+++ home-manager/terminal/dooit.py
@@ -1,10 +1,7 @@
-# Rose Pine Moon palette
+# rose pine Moon palette
 
 base = "#232136"
-surface = "#2a273f"
-overlay = "#393552"
-muted = "#6e6a86"
-subtle = "#908caa"
+muted = "#908caa"
 text = "#e0def4"
 love = "#eb6f92"
 gold = "#f6c177"
@@ -12,19 +9,21 @@ rose = "#ea9a97"
 pine = "#3e8fb0"
 foam = "#9ccfd8"
 iris = "#c4a7e7"
-hl-low = "#2a283e"
-hl-med = "#44415a"
-hl-high = "#56526e"
+
+# dooit's colors
+red = "#eb6f92"     #love
+yellow = "#f6c177"  #gold
+green = "#3e86b0"   #pine
 
 # Config
 BACKGROUND = base
-BAR-BACKGROUND = base
+BAR_BACKGROUND = base
 WORKSPACES_BACKGROUND = base
 TODOS_BACKGROUND = base
-BORDER_DIM = overlay
+BORDER_DIM = iris + " 50%"
 BORDER_LIT = rose
 
-BORDER_TITLE_DIM = subtle, base # fg then bg
+BORDER_TITLE_DIM = muted, base # fg then bg
 BORDER_TITLE_LIT = text, rose
 
 SEARCH_COLOR = love
@@ -36,49 +35,57 @@ USE_DATE_FIRST = True # True = dd-mm, False = mm-dd
 DATE_FORMAT = "%d %h"
 TIME_FORMAT = "%H:%M"
 
-DASHBOARD = [ART, QUOTE, PAD, STATS]
+# Dashboard
 # Art by Blazej Kozlowski
-ART = print(r"""\
-       _                        
-       \`*-.                    
-        )  _`-.                 
-       .  : `. .                
-       : _   '  \               
-       ; *` _.   `*-._          
-       `-.-'          `-.       
-         ;       `       `.     
-         :.       .        \    
-         . \  .   :   .-'   .   
-         '  `+.;  ;  '      :   
-         :  '  |    ;       ;-. 
-         ; '   : :`-:     _.`* ;
-[bug] .*' /  .*' ; .*`- +'  `*' 
-      `*-*   `*-*  `*-*'
-            """)
-QUOTE = [
-    "can you feel your heart burning?",
-    "can you feel the struggle within?",
-    "the fear within me is beyond anything your soul can make.",
-    "you cannot kill me in a way that matters",
-]
-PAD = " "
-STATS = [
-    f"Completed: {completed}",
-    f"Pending: {pending}",
-    f"Overdue: {overdue}",
+# Modified by mimvoid
+# source: https://www.asciiart.eu/animals/cats
+ART = [
+    "                              ",
+    "      .                      ",
+    "      \  ॱ` -.          ",
+    "       .  .-. `.                ",
+    "       : _   `  \               ",
+    "     _ ) *   .   `·-._          ",
+    "     ‾ `-.- '         `-.       ",
+    "         ;       `       `.     ",
+    "         :.       .        \    ",
+    "         . \  .   :   .-'   .   ",
+    "         '  `+.;  ;  '      :   ",
+    "         :  '  |    ;       ;-. ",
+    "        .  '   :  :`:     _.`· ;",
+    f"[{rose}]bug[/{rose}]   .·  .  .·' ; .·`- +'  `· ॱ",
+    "      `·-·   `·-·  `·-·'        ",
 ]
 
+# Didn't use an array since I wanted them centered
+# TODO: is there a shorthand for this?
+QUOTE1 = f"can you feel your [{love}]heart[/{love}] burning?"
+QUOTE2 = "can you feel the struggle within?"
+QUOTE3 = f"the fear within me is beyond [{pine}]anything[/{pine}] your soul can make."
+QUOTE4 = f"you [{love}]cannot kill me[/{love}] in a [{love}]way that matters[/{love}]"
+
+PAD = " "
+
+# TODO: get stats working
+#STATS = [
+#    f"Completed: {completed}",
+#    f"Pending: {pending}",
+#    f"Overdue: {overdue}",
+#]
+DASHBOARD = [ART, PAD, QUOTE1, QUOTE2, QUOTE3, QUOTE4]
+
+# Workspaces
 EMPTY_WORKSPACE = [
-    ":(",
+    ":O",
     "No workspaces?",
-    f"Press [{cyan}]'a'[/{cyan}] to add some!",
+    f"Press [{foam}]'a'[/{foam}] to add some!",
 ]
-WORKSPACE = [
+WORKSPACE = {
     "editing": pine,
-    "pointer": "> ",
+    "pointer": ">",
     "children_hint": " ›",
     "start_expanded": True,
-]
+}
 
 # Todos
 COLUMN_ORDER = ["description", "due", "urgency"]
@@ -87,10 +94,10 @@ EMPTY_TODO = [
     "Wow so empty!?",
     "Let's think of some stuff to do!"
 ]
-TODO = [
+TODO = {
     "color_todos": False, # color based on status
     "editing": pine,
-    "pointer": "> ",
+    "pointer": ">",
 
     "due_icon": "",
 
@@ -106,19 +113,19 @@ TODO = [
     "pending_icon": " ",
     "overdue_icon": " ",
 
-    "urgency1_icon": "󰂎",
+    "urgency1_icon": " ",
     "urgency2_icon": "󱊡",
     "urgency3_icon": "󱊢",
     "urgency4_icon": "󱊣!",
 
-    "urgency1_color": pine,
-    "urgency2_color": foam,
-    "urgency3_color": gold,
-    "urgency4_color": love,
+    "urgency1_color": "pine",
+    "urgency2_color": "foam",
+    "urgency3_color": "gold",
+    "urgency4_color": "love",
 
     "children_hint": "({done}/{total})",
     "start_expanded": True,
-]
+}
 
 #keybindings = [
 #    "move down": ["j", "<down>"],
