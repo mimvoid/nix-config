b8635343585a9ad96923f625b9425f666156bc41
mimvoid <mimvoid@proton.me>
2024-06-17 18:55:34 -0400

    truncated client title

--- home-manager/ags/main.ts
+++ home-manager/ags/main.ts
@@ -97,6 +97,8 @@ function ClientTitle() {
   return Widget.Label({
     class_name: "client-title",
     label: hyprland.active.client.bind("title"),
+    truncate: 'end',
+    maxWidthChars: 32,
   })
 }
 
