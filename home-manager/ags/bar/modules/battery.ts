c949cdb5b1ae3308f52d4aeeaf16d6aa21c3f7fb
mimvoid <mimvoid@proton.me>
2024-06-20 15:54:37 -0400

    worked on battery values

--- home-manager/ags/bar/modules/battery.ts
+++ home-manager/ags/bar/modules/battery.ts
@@ -3,10 +3,10 @@ const battery = await Service.import("battery")
 const icon = battery.bind("percent").as(p =>
   `battery-level-${Math.floor(p / 10) * 10}-symbolic`)
 
-const get_value = battery.bind("percent").as(p => p > 0 ? p / 100 : 0)
-
 const value = Widget.Revealer({
-  child: Widget.Label(` ${get_value}`),
+  child: Widget.Label({
+    label: battery.bind("percent").as(p => ` ${p}%`),
+  }),
 
   revealChild: false,
   transitionDuration: 250,
@@ -28,9 +28,10 @@ const eventbox = Widget.EventBox({
       value,
     ],
   }),
-})
 
-// FIX: battery percentage label does't show up right.
+  // FIXME: Displays the wrong value
+  tooltip_text: `Time left: ${battery.time_remaining}`,
+})
 
 export default () => Widget.Box({
   class_name: "battery",
