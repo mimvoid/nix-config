d3bfd4918c01be17004fb11012b70e2c984ba89d
mimvoid <mimvoid@proton.me>
2024-06-23 11:54:44 -0400

    added ags notification popups

--- home-manager/ags/main.ts
+++ home-manager/ags/main.ts
@@ -1,11 +1,13 @@
 import Bar from "./widgets/bar";
 import Calendar from "./widgets/calendar";
+import Notifications from "./widgets/notifications.ts";
 
 App.config({
   style: `/tmp/style/style.css`,
   windows: [
     Bar(),
     Calendar(),
+    Notifications(),
   ],
 })
 
