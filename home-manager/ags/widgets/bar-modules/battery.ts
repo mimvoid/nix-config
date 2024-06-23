c9d7963cd0f38acce5eada1eb9930af580dd7990
mimvoid <mimvoid@proton.me>
2024-06-22 23:07:38 -0400

    removed revealing from battery widget

--- home-manager/ags/widgets/bar-modules/battery.ts
+++ home-manager/ags/widgets/bar-modules/battery.ts
@@ -3,42 +3,64 @@ const battery = await Service.import("battery")
 const icon = battery.bind("percent").as(p =>
   `battery-level-${Math.floor(p / 10) * 10}-symbolic`)
 
-const value = Widget.Revealer({
-  child: Widget.Label({
-    label: battery.bind("percent").as(p => ` ${p}%`),
-  }),
-
-  revealChild: false,
-  transitionDuration: 250,
-  transition: 'slide_left',
+const value = Widget.Label({
+  label: battery.bind("percent").as(p => `${p}%`),
 })
 
-const eventbox = Widget.EventBox({
-  on_hover: () => {
-    value.reveal_child = true;
-    open = true;
-  },
-  on_hover_lost: () => {
-    value.reveal_child = false;
-    open = false;
-  },
-  child: Widget.Box({
-    children: [
-      Widget.Icon({
-        icon,
-        // Sets a class name to a charging battery, for styling
-        class_name: battery.bind('charging').as(ch => ch ? 'charging' : '')
-      }),
-      value,
-    ],
-  }),
+const battery_widget = Widget.Box({
+  children: [
+    Widget.Icon({
+      icon,
+      // Sets a class name to a charging battery for styling
+      class_name: battery.bind('charging').as(ch => ch ? 'charging' : '')
+    }),
+    value,
+  ],
 
-  // FIXME: Displays the wrong value
-  tooltip_text: `Time left: ${battery.time_remaining}`,
+  // TODO: this displays an absurdly high number of hours when
+  // when discharging with a nearly full battery,
+  // is it because of upower or something else?
+
+  tooltip_text: battery.bind('time-remaining').as(t => `Time left: ${Math.floor(t/60)}h ${t%60}m`),
 })
+
+// Uncomment to reveal percentage on hover
+
+//const value = Widget.Revealer({
+//  child: Widget.Label({
+//    label: battery.bind("percent").as(p => ` ${p}%`),
+//  }),
+//
+//  revealChild: false,
+//  transitionDuration: 250,
+//  transition: 'slide_left',
+//})
+//
+//const battery_widget = Widget.EventBox({
+//  on_hover: () => {
+//    value.reveal_child = true;
+//    open = true;
+//  },
+//  on_hover_lost: () => {
+//    value.reveal_child = false;
+//    open = false;
+//  },
+//  child: Widget.Box({
+//    children: [
+//      Widget.Icon({
+//        icon,
+//        // Sets a class name to a charging battery, for styling
+//        class_name: battery.bind('charging').as(ch => ch ? 'charging' : '')
+//      }),
+//      value,
+//    ],
+//  }),
+// 
+//  tooltip_text: `Time left: ${battery.time_remaining}`,
+//})
 
 export default () => Widget.Box({
   class_name: "battery",
   visible: battery.bind("available"),
-  child: eventbox,
+  child: battery_widget,
 })
