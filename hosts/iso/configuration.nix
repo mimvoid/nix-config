8c78cdc199936787a6b3ea9a05701dcc5e8aeef6
mimvoid <mimvoid@proton.me>
2024-06-28 12:28:15 -0400

    updated iso settings

--- hosts/iso/configuration.nix
+++ hosts/iso/configuration.nix
@@ -5,9 +5,13 @@
 
   nixpkgs.hostPlatform = "x86_64-linux";
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Disable wpa_supplicant for networkmanager
   networking = {
     wireless.enable = false;
     networkmanager.enable = true;
   };
+
+  console.font = "ter-d28b";
 }
