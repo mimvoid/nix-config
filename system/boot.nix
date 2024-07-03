5123ea15567edb3f510a44e9b2122c4f7864fcb3
mimvoid <mimvoid@proton.me>
2024-07-03 14:08:08 -0400

    removed grub config

--- system/boot.nix
+++ system/boot.nix
@@ -1,25 +1,8 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
   boot.loader = {
     efi.canTouchEfiVariables = true;
     systemd-boot.enable = true;
   };
-    
-#  boot.loader.grub = {
-#    enable = true;
-#    device = ''${config.fileSystems."/boot".device}'';
-#    efiSupport = true;
-#    useOSProber = true;
-
-#    # Default GRUB has such small text
-#    # This makes it more readable for me
-#    font = "${pkgs.fira-code}/share/fonts/truetype/FiraCode-VF.ttf";
-#    fontSize = 16; # Only works with ttf or otf fonts
-
-    #splashImage = 
-    #splashMode = "normal";
-#    backgroundColor = "#1e1e2e"; # Goes behind the splash image
-#    timeoutStyle = "menu";
-#  };
 }
