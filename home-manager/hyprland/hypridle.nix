12b5ec2febf00bfcffbb148ba8f4b6fdf960937d
mimvoid <mimvoid@proton.me>
2024-07-03 14:07:57 -0400

    adjusted indentation

--- home-manager/hyprland/hypridle.nix
+++ home-manager/hyprland/hypridle.nix
@@ -1,37 +1,37 @@
 {
-    services.hypridle = {
-        enable = true;
-        settings = {
-            general = {
-                lock_cmd = "hyprlock";
-                before_sleep_cmd = "loginctl lock-session";
-                after_sleep_cmd = "hyprctl dispatch dpms on";
-            };
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
 
-            listener = [
-                {
-                    # Idle warning
-                    timeout = 540;
-                    on-timeout = "notify-send 'Hypridle' 'Are you there...?'";
-                    on-resume = "notify-send 'Hypridle' 'Welcome back!'";
-                }
-                {
-                    # Screen locking
-                    timeout = 600;
-                    on-timeout = "hyprlock";
-                }
-                {
-                    # Screen off
-                    timeout = 900;
-                    on-timeout = "hyprctl dispatch dpms off";
-                    on-resume = "hyprctl dispatch dpms on";
-                }
-                {
-                    # Suspend
-                    timeout = 1200;
-                    on-timeout = "systemctl suspend";
-                }
-            ];
-        };
+      listener = [
+        {
+          # Idle warning
+          timeout = 540;
+          on-timeout = "notify-send 'Hypridle' 'Are you there...?'";
+          on-resume = "notify-send 'Hypridle' 'Welcome back!'";
+        }
+        {
+          # Screen locking
+          timeout = 600;
+          on-timeout = "hyprlock";
+        }
+        {
+          # Screen off
+          timeout = 900;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+        {
+          # Suspend
+          timeout = 1200;
+          on-timeout = "systemctl suspend";
+        }
+      ];
     };
+  };
 }
--- home-manager/terminal/foot.nix
+++ home-manager/terminal/foot.nix
@@ -1,21 +1,21 @@
 # Colors and fonts are managed by Stylix
 {
-    programs.foot = {
-        enable = true;
-        settings = {
-            main.pad = "12x0";
-            scrollback = {
-                lines = 1000;
-                indicator-position = "fixed";
-                indicator-format = "percentage";
-            };
-            cursor = {
-                style = "block";
-                unfocused-style = "hollow";
-            };
-            mouse = {
-                hide-when-typing = "yes";
-            };
-        };
+  programs.foot = {
+   enable = true;
+    settings = {
+      main.pad = "12x0";
+      scrollback = {
+        lines = 1000;
+        indicator-position = "fixed";
+        indicator-format = "percentage";
+      };
+      cursor = {
+        style = "block";
+        unfocused-style = "hollow";
+      };
+      mouse = {
+        hide-when-typing = "yes";
+      };
     };
+  };
 }
