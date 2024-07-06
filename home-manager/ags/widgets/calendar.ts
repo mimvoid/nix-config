8a53b1020b45eda2b4a1f3625352cd980392c959
mimvoid <mimvoid@proton.me>
2024-07-05 22:39:11 -0400

    updated ags calendar config

--- home-manager/ags/widgets/calendar.ts
+++ home-manager/ags/widgets/calendar.ts
@@ -1,20 +1,18 @@
 const calendar = Widget.Calendar({
   show_day_names: true,
-  //show_details: true,
   show_heading: true,
   show_week_numbers: true,
-  //detail: (self, y, m, d) => {
-  //  return `<span color="white">${y}. ${m}. ${d}.</span>`
-  //},
-  on_day_selected: ({ date: [y, m, d] }) => {
-    print(`${y}. ${m}. ${d}.`)
-  },
+})
+
+const calendar_box = Widget.Box({
+  class_name: "calendar",
+  child: calendar,
 })
 
 // FIX: toggling with reveal_child: false doesn't show the calendar
 
-const calendar_box = Widget.Revealer({
-  child: calendar,
+const calendar_revealer = Widget.Revealer({
+  child: calendar_box,
   reveal_child: true,
   transition: "slide_down",
   transition_duration: 200,
@@ -27,10 +25,10 @@ const calendar_box = Widget.Revealer({
 export default (monitor: number) => Widget.Window({
   monitor,
   name: "calendar",
-  class_name: "calendar",
+  class_name: "calendar-window",
   anchor: ["top"],
   exclusivity: "normal",
   layer: "overlay",
   margins: [6, 0],
-  child: calendar_box,
+  child: calendar_revealer,
 })
