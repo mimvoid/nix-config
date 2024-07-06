6436e60eb9cae7dc41ebd942b3c858d66ff2ead6
mimvoid <mimvoid@proton.me>
2024-07-06 01:44:35 -0400

    adjusted brightness widget revealer

--- home-manager/ags/widgets/bar-modules/brightness.ts
+++ home-manager/ags/widgets/bar-modules/brightness.ts
@@ -4,42 +4,46 @@ import WlsunsetToggle from './wlsunset';
 const slider = Widget.Slider({
   on_change: self => brightness.screen_value = self.value,
   value: brightness.bind('screen-value'),
-
-  hexpand: true,
-  vpack: "center",
   draw_value: false,
-});
-
-const slider_box = Widget.Revealer({
-  child: slider,
-
   cursor: "pointer",
-  reveal_child: false,
-  transition_duration: 250,
-  transition: 'slide_left',
-})
+});
 
 const label = Widget.Label({
   label: brightness.bind('screen-value').as(v => `${Math.floor(v * 100)}%`),
   setup: self => self.hook(brightness, (self, screenValue) => {
     // screenValue is the passed parameter from the 'screen-changed' signal
-    self.label = screenValue ?? 0;
+    self.label = screenValue ?? `0`;
 
     self.label = `${brightness.screenValue}`;
   }, 'screen-changed'),
 });
 
+const brightness_elements = Widget.Box({
+  children: [ slider, label ],
+  hexpand: true,
+  vpack: "center",
+})
+
+const revealer = Widget.Revealer({
+  child: brightness_elements,
+
+  reveal_child: false,
+  transition_duration: 250,
+  transition: 'slide_left',
+})
+
 const eventbox = Widget.EventBox({
   on_hover: () => {
-    slider_box.reveal_child = true;
+    revealer.reveal_child = true;
   },
   on_hover_lost: () => {
-    slider_box.reveal_child = false;
+    revealer.reveal_child = false;
   },
 
   child: Widget.Box({
-    children: [slider_box, label],
-  }), 
+    child: revealer,
+    class_name: "brightness-revealer",
+  }),
 })
 
 export default () => Widget.Box({
