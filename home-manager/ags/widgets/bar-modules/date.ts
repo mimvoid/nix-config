58a040e040198950bb9fb6dc496b5660cbb91827
mimvoid <mimvoid@proton.me>
2024-07-07 13:17:36 -0400

    changed date widget cursor

--- home-manager/ags/widgets/bar-modules/date.ts
+++ home-manager/ags/widgets/bar-modules/date.ts
@@ -6,4 +6,5 @@ export default () => Widget.Button({
   child: Widget.Label({ label: date.bind() }),
   class_name: "clock",
   on_clicked: () => App.toggleWindow("calendar"),
+  cursor: "pointer",
 })
