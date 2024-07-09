5142f2819f2c8ed7191f22bb90892d2b51d0558e
mimvoid <mimvoid@proton.me>
2024-07-08 20:26:29 -0400

    updated xterm config

--- home-manager/terminal/default.nix
+++ home-manager/terminal/default.nix
@@ -16,4 +16,16 @@
     gotop
     unstable.dooit
   ];
+
+  # Theming-specific config is in ../theming.nix
+  xresources.properties = {
+    "XTerm.termName" = "xterm-256color";
+    "XTerm*locale" = true;
+    "XTerm*metaSendsEscape" = true;
+    "XTerm*selectToClipboard" = true;
+    "XTerm*translations" = ''#override \n\
+      Ctrl Shift <Key>C: copy-selection(CLIPBOARD) \n\
+      Ctrl Shift <Key>V: insert-selection(CLIPBOARD)
+    '';
+  };
 }
