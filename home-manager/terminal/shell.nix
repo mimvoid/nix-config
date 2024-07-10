b2baf7687e552777afffcf5ffba9eb6e8b271c17
mimvoid <mimvoid@proton.me>
2024-07-09 21:50:02 -0400

    cleaned up shell config

--- home-manager/terminal/shell.nix
+++ home-manager/terminal/shell.nix
@@ -3,7 +3,6 @@
 {
   home.packages = with pkgs; [
     # zsh
-    zsh
     zsh-z
     zsh-autopair
     zsh-syntax-highlighting
@@ -15,9 +14,7 @@
 
     # misc
     eza
-    starship
     disfetch
-    fzf
     tlrc
     exiftool
     megacmd
@@ -25,42 +22,37 @@
 
   programs.bash = {
     enable = true;
-    #enableCompletion = true;
+    enableCompletion = true;
     historyControl = [ "erasedups" "ignoredups" ];
-    historyIgnore = [ "ls" "cd" "exit" "ya" ];
+    historyIgnore = [ "ls" "cd" "exit" ];
   };
 
   programs.zsh = {
     enable = true;
     dotDir = "zsh";
+    syntaxHighlighting.enable = true;
+    
     history = {
       path = "$HOME/zsh/.zsh_history";
       ignoreDups = true;
       ignorePatterns = [ "ls" "eza" "exit" "yazi" "yy" ];
-      ignoreSpace = true;
     };
-    syntaxHighlighting = {
-      enable = true;
-    };
+    
     historySubstringSearch = {
       enable = true;
       searchUpKey = "$terminfo[kcuu1]";
       searchDownKey = "$terminfo[kcud1]";
     };
+    
     shellAliases = {
-      ya = "yy";
-
-      gco = "git checkout";
-      gpull = "git pull";
-      gpush = "git push";
-      ga = "git add";
-
-      nhh = "nh home switch ~/NixOS";
       nhos = "nh os switch ~/NixOS";
+      nhh = "nh home switch ~/NixOS";
+      
+      ya = "yy";
     };
+    
     completionInit = " autoload -U compinit && compinit";
-    initExtra =
-    ''
+    initExtra = ''
       source $HOME/zsh/plugins/zsh-autopair/share/zsh/zsh-autopair/autopair.zsh
       source $HOME/zsh/plugins/zsh-z/share/zsh-z/zsh-z.plugin.zsh
       source $HOME/zsh/plugins/zsh-z
