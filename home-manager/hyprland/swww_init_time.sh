52442daa702d52b50295104996e9a7b7d7601de8
mimvoid <mimvoid@proton.me>
2024-07-15 13:59:07 -0400

    updated wallpapers directory

--- home-manager/hyprland/swww_init_time.sh
+++ home-manager/hyprland/swww_init_time.sh
@@ -3,22 +3,22 @@
 case $(date +%H) in
 	00 | 01 | 02 | 03 | 04 | 05 | 06) # 0:00 to 6:59, early morning
 
-		swww img "$HOME"/NixOS/wallpapers/gracile_twilight.jpg
+		swww img "$HOME"/NixOS/wallpapers/wallpapers/gracile_twilight.jpg
     ;;
   07 | 08 | 09 | 10 | 11) # 7:00 to 11:59, morning
 		
-    swww img "$HOME"/NixOS/wallpapers/dangerdrop_coffee-roadtrip.jpg
+    swww img "$HOME"/NixOS/wallpapers/wallpapers/dangerdrop_coffee-roadtrip.jpg
     ;;
 	12 | 13 | 14 | 15) # 12:00 to 15:59, midday
 
-		swww img "$HOME"/NixOS/wallpapers/dangerdrop_coffee-roadtrip.jpg
+		swww img "$HOME"/NixOS/wallpapers/wallpapers/dangerdrop_coffee-roadtrip.jpg
 		;;
 	16 | 17 | 18 | 19 | 20) # 16:00 to 19:59, late afternoon/evening
 
-    swww img "$HOME"/NixOS/wallpapers/dangerdrop_coffee-roadtrip.jpg
+    swww img "$HOME"/NixOS/wallpapers/wallpapers/dangerdrop_coffee-roadtrip.jpg
     ;;
   21 | 22 | 23) # 21:00 to 23:59, evening/nighttime
 
-		swww img "$HOME"/NixOS/wallpapers/gracile_overgrown.jpg
+		swww img "$HOME"/NixOS/wallpapers/wallpapers/gracile_overgrown.jpg
 		;;
 esac
