6fba6afa6347203a0e9afd394ab9c0e8aa7e5536
mimvoid <mimvoid@proton.me>
2024-07-16 23:56:09 -0400

    gave flatpaks access to themes

--- home-manager/guis/flatpaks.nix
+++ home-manager/guis/flatpaks.nix
@@ -34,6 +34,13 @@
     };
 
     overrides.global = {
+      # Give access to GTK themes & icons
+      Context.filesystems = [
+        "xdg-config/gtk-3.0:ro"
+        "xdg-config/gtk-4.0:ro"
+        "xdg-data/icons:ro"
+      ];
+
       # Fix unthemed cursor in some Wayland apps
       Environment.XCURSOR_PATH = "/run/host/user-share/icons:/run/host/share/icons";
     };
