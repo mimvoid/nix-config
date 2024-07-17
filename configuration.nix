0de8aad661ba49da61100b065d7c988bce74d020
mimvoid <mimvoid@proton.me>
2024-07-16 23:53:26 -0400

    added tlp & auto-cpufreq

--- configuration.nix
+++ configuration.nix
@@ -140,7 +140,9 @@
     percentageAction = 5;
     criticalPowerAction = "HybridSleep";
   };
-  services.power-profiles-daemon.enable = true;
+
+  services.tlp.enable = true;
+  services.auto-cpufreq.enable = true;
 
   # Networks & connections
   networking = {
