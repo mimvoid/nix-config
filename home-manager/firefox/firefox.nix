65350414aebd7040e7df32e53f9cb1ebfe37ef18
mimvoid <mimvoid@proton.me>
2024-08-12 20:27:08 -0400

    delete unused files & packages

--- home-manager/firefox/firefox.nix
+++ home-manager/firefox/firefox.nix
@@ -1,11 +1,8 @@
 { pkgs, ... }:
 
 {
-  home.packages = [ pkgs.unstable.firefoxpwa ];
-  
   programs.firefox = {
     enable = true;
     package = pkgs.firefox;
-    nativeMessagingHosts = [ pkgs.unstable.firefoxpwa ];
   };
 }
