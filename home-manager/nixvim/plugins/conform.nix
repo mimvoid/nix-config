460a9a65f7fd984b09a2370c4463f6a776c0fa5e
mimvoid <mimvoid@proton.me>
2024-08-28 13:40:05 -0400

    add conform nvim

---
+++ home-manager/nixvim/plugins/conform.nix
@@ -0,0 +1,35 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    prettierd
+    shellcheck-minimal
+    shellharden
+    shfmt
+    black
+    isort
+  ];
+
+  programs.nixvim.plugins.conform-nvim = {
+    enable = true;
+
+    formatOnSave.lspFallback = true;
+
+    formattersByFt = {
+      javascript = [[ "prettierd" "prettier" ]];
+      typescript = [[ "prettierd" "prettier" ]];
+      
+      css = [[ "prettierd" "prettier" ]];
+      scss = [[ "prettierd" "prettier" ]];
+      
+      html = [[ "prettierd" "prettier" ]];
+      markdown = [[ "prettierd" "prettier" ]];
+      
+      bash = [ "shellcheck" "shellharden" "shfmt" ];
+      #go = [ "goimports" "gofmt" ];
+      python = [ "black" "isort" ];
+      
+      "_" = [ "squeeze_blanks" "trim_whitespace" "trim_newlines" ];
+    };
+  };
+}
--- home-manager/nixvim/plugins/default.nix
+++ home-manager/nixvim/plugins/default.nix
@@ -5,6 +5,7 @@
     ./alpha.nix
     ./bufferline.nix
     ./completion.nix
+    ./conform.nix
     ./dap.nix
     ./git.nix
     ./lsp.nix
