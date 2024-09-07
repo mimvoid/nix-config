a57ffd94c363721e08be330f60cf98c2fababa94
mimvoid <mimvoid@proton.me>
2024-09-07 01:37:27 -0400

    add R.nvim

--- home-manager/nixvim/plugins/default.nix
+++ home-manager/nixvim/plugins/default.nix
@@ -1,5 +1,15 @@
 { pkgs, ... }:
-
+let
+  r-nvim = pkgs.vimUtils.buildVimPlugin {
+    name = "R.nvim";
+    src = pkgs.fetchFromGitHub {
+      owner = "R-nvim";
+      repo = "R.nvim";
+      rev = "0acf1fc99a504cf45f3b91dd9ec9394ec73f33fe";
+      hash = "sha256-FHjjae4QhohHeZQIzCvZZ3SoP3oxrNm9g6kjYMc0AAo=";
+    };
+  };
+in
 {
   imports = [
     ./alpha.nix
@@ -30,7 +40,7 @@
     extraPlugins = with pkgs.vimPlugins; [
       plenary-nvim
       fcitx-vim
-    ];
+    ] ++ [ r-nvim ];
 
     extraPackages = with pkgs; [
       fzf
