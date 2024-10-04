e2b06c8075fe74b109bf12e02d6388e8b5b64276
mimvoid <mimvoid@proton.me>
2024-10-04 01:49:19 -0400

    add neotab.nvim

--- home-manager/nixvim/plugins/completion.nix
+++ home-manager/nixvim/plugins/completion.nix
@@ -10,7 +10,7 @@
       mapping = {
         "<CR>" = "cmp.mapping.confirm({ select = true })";
         "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-        "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+        # "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
       };
       sources = [
         { name = "path"; }
--- home-manager/nixvim/plugins/default.nix
+++ home-manager/nixvim/plugins/default.nix
@@ -9,6 +9,7 @@ let
       hash = "sha256-FHjjae4QhohHeZQIzCvZZ3SoP3oxrNm9g6kjYMc0AAo=";
     };
   };
+
   nvim-project-marks = pkgs.vimUtils.buildVimPlugin {
     name = "nvim-project-marks";
     src = pkgs.fetchFromGitHub {
@@ -16,6 +17,16 @@ let
       repo = "nvim-project-marks";
       rev = "0.2.0";
       hash = "sha256-/LC/URb4sEURa7S9Ht20k9eTbJ3tZCvkHRwcxlBfra0=";
+    };
+  };
+
+  neotab-nvim = pkgs.vimUtils.buildVimPlugin {
+    name = "neotab.nvim";
+    src = pkgs.fetchFromGitHub {
+      owner = "kawre";
+      repo = "neotab.nvim";
+      rev = "6c6107dddaa051504e433608f59eca606138269b";
+      hash = "sha256-bSFKbjj8fJHdfBzYoQ9l3NU0GAYfdfCbESKbwdbLNSw=";
     };
   };
 in
@@ -43,14 +54,39 @@ in
 
     extraPlugins = with pkgs.vimPlugins; [
       plenary-nvim
+      tabout-nvim
       fcitx-vim
     ] ++ [
       r-nvim
       nvim-project-marks
+      neotab-nvim
     ];
 
-    extraConfigLua = ''
-      require('projectmarks').setup({})
+    extraConfigLua = # lua
+    ''
+      require("projectmarks").setup({})
+
+      require("neotab").setup({
+        tabkey = "",
+        smart_punctuators = {
+          enabled = true,
+          semicolon = {
+            enabled = true,
+            ft = { "nix", "javascript", "typescript" },
+          },
+        },
+      })
+    '';
+
+    plugins.cmp.settings.mapping."<Tab>" = # lua
+    ''
+      cmp.mapping(function()
+        if cmp.visible() then
+          cmp.select_next_item()
+        else
+          require("neotab").tabout()
+        end
+      end)
     '';
 
     extraPackages = with pkgs; [
