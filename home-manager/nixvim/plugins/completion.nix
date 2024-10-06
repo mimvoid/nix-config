0edc01e171e6f062b83d27cdf58dd87758145c84
mimvoid <mimvoid@proton.me>
2024-10-06 16:03:24 -0400

    add more neovim keybindings

--- home-manager/nixvim/plugins/completion.nix
+++ home-manager/nixvim/plugins/completion.nix
@@ -11,6 +11,7 @@
         "<CR>" = "cmp.mapping.confirm({ select = true })";
         "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
         # "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+        "<C-e>" = "cmp.mapping.abort()";
       };
       sources = [
         { name = "path"; }
--- home-manager/nixvim/plugins/lsp.nix
+++ home-manager/nixvim/plugins/lsp.nix
@@ -1,6 +1,21 @@
 {
   programs.nixvim.plugins.lsp = {
     enable = true;
+    
+    keymaps = {
+      diagnostic = {
+        "<leader>j" = "goto_next";
+        "<leader>k" = "goto_prev";
+      };
+      lspBuf = {
+        K = "hover";
+        gD = "references";
+        gd = "definition";
+        gi = "implementation";
+        gt = "type_definition";
+      };
+    };
+
     servers = {
       bashls.enable = true;
       # basedpyright.enable = true;
