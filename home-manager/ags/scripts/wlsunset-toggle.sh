bf5fac9df5aecd1bfa5e2b651da068500134ed9f
mimvoid <mimvoid@proton.me>
2024-10-17 00:09:18 -0400

    adjust wlsunset args

--- home-manager/ags/scripts/wlsunset-toggle.sh
+++ home-manager/ags/scripts/wlsunset-toggle.sh
@@ -2,7 +2,7 @@
 
 if pgrep -x "wlsunset" > /dev/null
 then
-  pkill wlsunset > /dev/null 2>&1
+  systemctl --user stop wlsunset.service > /dev/null 2>&1
 else
-  wlsunset -T 5000 -t 2500 -S 7:30 -s 20:00 > /dev/null 2>&1 &
+  systemctl --user start wlsunset.service > /dev/null 2>&1 &
 fi
--- home-manager/hyprland/wlsunset.nix
+++ home-manager/hyprland/wlsunset.nix
@@ -1,17 +1,42 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   # Allows wlsunset to launch with a terminal command
   home.packages = [ pkgs.wlsunset ];
 
-  services.wlsunset = {
-    enable = true;
-    systemdTarget = "hyprland-session.target";
-    sunrise = "7:30";
-    sunset = "20:00";
-    temperature = {
-      day = 5000;
-      night = 2500;
+  # Set up custom args (duration)
+  systemd.user.services.wlsunset = {
+    Unit = {
+      Description = "Day/night gamma adjustments for Wayland compositors.";
+      PartOf = [ "graphical-session.target" ];
     };
+
+    Service = {
+      ExecStart = let
+        args = lib.cli.toGNUCommandLineShell { } {
+          # Daytime
+          T = 5000;
+          S = "7:30";
+          # Nighttime
+          t = 2500;
+          s = "20:00";
+          # Duration
+          d = 1800;
+        };
+      in "${pkgs.wlsunset}/bin/wlsunset ${args}";
+    };
+
+    Install = { WantedBy = [ "hyprland-session.target" ]; };
   };
+
+  # services.wlsunset = {
+  #   enable = true;
+  #   systemdTarget = "hyprland-session.target";
+  #   sunrise = "7:30";
+  #   sunset = "20:00";
+  #   temperature = {
+  #     day = 5000;
+  #     night = 2500;
+  #   };
+  # };
 }
