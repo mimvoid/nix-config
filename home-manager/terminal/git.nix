e16cfcc334f9b6a60ac071bf1b5ea16199b70b34
mimvoid <mimvoid@proton.me>
2024-11-01 23:31:13 -0400

    update lazygit settings

--- home-manager/terminal/git.nix
+++ home-manager/terminal/git.nix
@@ -11,7 +11,28 @@
       init.defaultBranch = "main";
       core.editor = "nvim";
     };
+
+    diff-so-fancy = {
+      enable = true;
+      stripLeadingSymbols = false;
+      markEmptyLines = false;
+    };
   };
 
-  programs.lazygit.enable = true;
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      gui = {
+        scrollPastBottom = false;
+        showRandomTip = false;
+        showListFooter = false;
+        showPanelJumps = false;
+        filterMode = "fuzzy";
+        showDivergenceFromBaseBranch = "onlyArrow";
+        statusPanelView = "allBranchesLog";
+      };
+      git.paging.pager = "diff-so-fancy";
+      notARepository = "quit";
+    };
+  };
 }
