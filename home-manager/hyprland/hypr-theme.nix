bf2b023bf9ff2ae3910a765314b489c9bdab987e
mimvoid <mimvoid@proton.me>
2024-11-05 10:52:24 -0500

    fix swww's initial wallpaper when starting hyprland

--- home-manager/hyprland/hypr-theme.nix
+++ home-manager/hyprland/hypr-theme.nix
@@ -1,8 +1,35 @@
-{ config, lib, ... }:
+{ pkgs, config, lib, ... }:
 
 let
   pal = config.palette;
   type = config.stylix.fonts;
+
+  wallpapers = "${config.home.homeDirectory}/NixOS/wallpapers/wallpapers";
+  swww-schedule = pkgs.writeShellScript "swww-schedule" # bash
+    ''
+      case $(date +%H) in
+        00 | 01 | 02 | 03 | 04 | 05 | 06) # 0:00 to 6:59, early morning
+
+          swww-daemon ; swww img ${wallpapers}/gracile_twilight.jpg
+          ;;
+        07 | 08 | 09 | 10 | 11) # 7:00 to 11:59, morning
+
+          swww-daemon ; swww img ${wallpapers}/dangerdrop_coffee-roadtrip.jpg
+          ;;
+        12 | 13 | 14 | 15) # 12:00 to 15:59, midday
+
+          swww-daemon ; swww img ${wallpapers}/dangerdrop_coffee-roadtrip.jpg
+          ;;
+        16 | 17 | 18 | 19 | 20) # 16:00 to 19:59, late afternoon/evening
+
+          swww-daemon ; swww img ${wallpapers}/dangerdrop_coffee-roadtrip.jpg
+          ;;
+        21 | 22 | 23) # 21:00 to 23:59, evening/nighttime
+
+          swww-daemon ; swww img ${wallpapers}/gracile_overgrown.jpg
+          ;;
+      esac
+    '';
 in
 {
   imports = [
@@ -12,8 +39,8 @@ in
 
   wayland.windowManager.hyprland.settings = {
     # Homescreen wallpaper
-    exec-once = lib.mkAfter [ "swww-daemon --no-cache &" ];
-    exec = lib.mkAfter [ "${./swww_init_time.sh} &" ];
+    exec = lib.mkAfter [ "${swww-schedule} &" ];
+
     general = {
       "col.active_border" = "rgb(${pal.primary})";
       "col.inactive_border" = "rgba(${pal.primary-dim}40)"; # 25% opacity
