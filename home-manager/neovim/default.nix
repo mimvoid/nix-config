c65593ba75a2cdb4af013e6bbd8c89bde2116591
mimvoid <mimvoid@proton.me>
2024-11-19 20:14:58 -0500

    nvim: properly override desktop entry to launch with kitty

--- home-manager/neovim/default.nix
+++ home-manager/neovim/default.nix
@@ -1,7 +1,37 @@
 { pkgs, inputs, ... }:
+let
+  # Get nixPatch Neovim from inputs
+  nvim = inputs.nvim.packages.${pkgs.system}.default;
 
+  # Copy and override the Neovim desktop entry
+  nvim-kitty-desktop =
+    with pkgs;
+    let
+      contents =
+        builtins.replaceStrings
+          [
+            "Terminal=true"
+            "Exec=nvim %F"
+          ]
+          [
+            "Terminal=false"
+            "Exec=kitty nvim %F"
+          ]
+          (builtins.readFile "${nvim}/share/applications/nvim.desktop");
+    in
+    lib.hiPrio (writeTextFile {
+      name = "nvim.desktop";
+      destination = "/share/applications/nvim.desktop";
+      text = contents;
+      checkPhase = ''${buildPackages.desktop-file-utils}/bin/desktop-file-validate "$target"'';
+    });
+in
 {
-  home.packages = with pkgs; [ (inputs.nvim.packages.${system}.default) ];
+  home.packages = [
+    nvim
+    nvim-kitty-desktop
+  ];
 
+  # Make default editor
   home.sessionVariables.EDITOR = "nvim";
 }
--- home-manager/terminal/kitty.nix
+++ home-manager/terminal/kitty.nix
@@ -39,33 +39,4 @@
       mouse_hide_wait -1
     '';
   };
-
-  # HACK: override neovim wrapper terminal
-  xdg.desktopEntries.nvim = {
-    exec = "kitty nvim %F";
-    terminal = false;
-
-    name = "Neovim wrapper";
-    genericName = "Text Editor";
-    comment = "Edit text files";
-    icon = "nvim";
-    categories = [ "Utility" "TextEditor" ];
-    mimeType = [
-      "text/english"
-      "text/plain"
-      "text/x-makefile"
-      "text/x-c++hdr"
-      "text/x-c++src"
-      "text/x-chdr"
-      "text/x-csrc"
-      "text/x-java"
-      "text/x-moc"
-      "text/x-pascal"
-      "text/x-tcl"
-      "text/x-tex"
-      "application/x-shellscript"
-      "text/x-c"
-      "text/x-c++"
-    ];
-  };
 }
