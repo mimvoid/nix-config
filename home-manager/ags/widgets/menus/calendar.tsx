f36a6679de4cd55ae6ee40c60e7a4388b7420167
mimvoid <mimvoid@proton.me>
2024-12-24 16:27:20 -0500

    ags: rewrite calendar window clock

--- home-manager/ags/widgets/menus/calendar.tsx
+++ home-manager/ags/widgets/menus/calendar.tsx
@@ -1,45 +1,62 @@
 import { Variable } from "astal";
-import {
-  App,
-  Astal,
-  Gtk,
-  Gdk,
-  Widget,
-  astalify,
-  type ConstructProps,
-} from "astal/gtk3";
+import { App, Astal, Gtk, astalify, type ConstructProps } from "astal/gtk3";
 import GObject from "gi://GObject";
-
-// Set up calendar widget
-class Calendar extends astalify(Gtk.Calendar) {
-  static {
-    GObject.registerClass(this);
-  }
-
-  constructor(
-    props: ConstructProps<Calendar, Gtk.Calendar.ConstructorProps, {}>,
-  ) {
-    super(props as any);
-  }
-}
 
 const CalendarWidget = () => {
   // Format time with seconds
-  const timeSeconds = Variable<string>("").poll(1000, "date '+%H:%M:%S'");
-  const Time = <label className="display in-box" label={timeSeconds()} />;
+  function Time() {
+    function getTime(time: string, className: string) {
+      const polled = Variable<string>("").poll(1000, `date '+${time}'`);
+      return (
+        <label
+          label={polled()}
+          className={`display ${className}`}
+          valign={Gtk.Align.END}
+          hexpand
+          vexpand
+        />
+      );
+    }
 
-  // Make the calendar
-  const Content = <Calendar showHeading showDayNames showWeekNumbers />;
+    const times = [
+      ["%H", "hour"],
+      ["%M", "minute"],
+      ["%S", "second"],
+    ]
+
+    return (
+      <box className="big-clock" halign={Gtk.Align.CENTER} hexpand>
+        {times.map((time) => getTime(time[0], time[1]))}
+      </box>
+    );
+  }
+
+  function Calendar() {
+    // Set up calendar widget
+    class Calendar extends astalify(Gtk.Calendar) {
+      static {
+        GObject.registerClass(this);
+      }
+
+      constructor(
+        props: ConstructProps<Calendar, Gtk.Calendar.ConstructorProps, {}>,
+      ) {
+        super(props as any);
+      }
+    }
+
+    return <Calendar showHeading showDayNames showWeekNumbers />;
+  }
 
   return (
     <box className="calendar box" vertical>
-      {Time}
-      {Content}
+      <Time />
+      <Calendar />
     </box>
   );
 };
 
-export default function Cal() {
+export default function Calendar() {
   return (
     <window
       name="calendar"
