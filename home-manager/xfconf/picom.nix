c19d589ecff96d0cd8f47e4bc93b42608f1b5026
mimvoid <mimvoid@proton.me>
2025-05-24 16:35:16 -0400

    update picom config

--- home-manager/xfconf/picom.nix
+++ home-manager/xfconf/picom.nix
@@ -1,108 +1,138 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
+  home.packages = with pkgs; [ unstable.picom ];
+
   xfconf.settings.xfwm4."general/use_compositing" = false;
 
-  services.picom = {
-    enable = true;
-    package = pkgs.unstable.picom;
+  xdg.configFile."picom/picom.conf".text = ''
     backend = "glx";
+    vsync = true;
 
-    fade = true;
-    fadeDelta = 2;
+    fading = true;
+    fade-delta = 2;
+    fade-in-step = 0.028;
+    fade-out-step = 0.03;
 
-    activeOpacity = 0.85;
-    inactiveOpacity = 0.7;
-    menuOpacity = 0.85;
-    opacityRules = [
-      "100:fullscreen"
-      "100:class_g = 'firefox' && focused"
-      "100:class_g = 'Vivaldi-stable' && focused"
-      "100:class_g = 'krita'"
-      "100:class_g *?= 'ristretto'"
-      "100:class_g = 'zoom'"
-      "100:class_g *?= 'virt-manager'"
-      "100:class_g *?= 'org.kde.digikam'"
-      "98:class_g *?= 'obsidian' && focused"
-      "98:class_g *?= 'zotero' && focused"
-      "80:class_g = 'kitty' && focused"
-    ];
+    blur: {
+      method = "dual_kawase";
+      strength = 1;
+    }
 
-    shadow = true;
-    shadowOpacity = 0.5;
-    shadowOffsets = [ (-20) (-20) ];
-    shadowExclude =
-      lib.lists.map (i: "window_type *= '${i}'") [
-        "menu"
-        "dropdown_menu"
-        "popup_menu"
-        "utility"
-        "notification"
-        "dock"
-        "toolbar"
-        "tooltip"
-      ]
-      ++ [ "class_g = 'firefox' && argb" ];
+    shadow = false;
+    shadow-offset-x = -20;
+    shadow-offset-y = -20;
+    shadow-opacity = 0.5;
 
-    vSync = true;
+    detect-client-leader = false;
+    detect-client-opacity = true;
+    detect-rounded-corners = true;
+    detect-transient = true;
+    mark-wmmin-focused = true;
+    unredir-if-possible = true;
 
-    settings = {
-      corner-radius = 10;
-      rounded-corners-exclude = [ "window_type = 'desktop'" ];
+    rules = (
+      { opacity = 0.85; corner-radius = 10; },
+      {
+        match = "window_type = 'normal' && !focused";
+        opacity = 0.7;
+      },
+      {
+        # Opaque windows
+        match = "fullscreen || "
+                "class_g = 'firefox' || "
+                "class_g = 'krita' || "
+                "class_g *?= 'ristretto' || "
+                "class_g = 'zoom' || "
+                "class_g *?= 'virt-manager' || "
+                "class_g *?= 'org.kde.digikam'";
+        opacity = 1.0;
+        full-shadow = false;
+        blur-background = false;
+      },
+      {
+        # Almost opaque windows
+        match = "(class_g *?= 'obsidian' || class_g *?= 'zotero') && focused";
+        opacity = 0.95;
+      },
+      {
+        # Terminal
+        match = "class_g = 'kitty' && focused";
+        opacity = 0.8;
+      },
+      {
+        match = "class_g = 'firefox'";
+        blur-background = false;
+      },
+      {
+        match = "class_g = 'firefox' && argb";
+        shadow = false;
+      },
 
-      blur = {
-        method = "dual_kawase";
-        strength = 1;
-      };
-      blur-exclude = [ "class_g = 'firefox'" ];
+      # Window types
+      {
+        match = "window_type = 'desktop'";
+        corner-radius = 0;
+      },
+      {
+        match = "window_type = 'dropdown_menu' || "
+                "window_type = 'popup_menu'";
+        opacity = 0.85;
+      },
+      {
+        match = "window_type = 'notification'";
+        animations = (
+          {
+            triggers = [ "open", "show" ];
+            preset = "fly-in";
+            direction = "left";
+          },
+          {
+            triggers = [ "close", "hide" ];
+            preset = "fly-out";
+            direction = "right";
+          }
+        )
+      },
+      {
+        match = "window_type = 'normal'";
+        shadow = true;
+        animations = ({
+          triggers = [ "close", "hide", "open", "show", "geometry" ];
 
-      mark-wmwin-focused = true;
-      mark-ovredir-focused = true;
-      detect-rounded-corners = true;
-      detect-client-opacity = true;
-      unredir-if-possible = false;
-      detect-transient = true;
-      detect-client-leader = false;
-    };
-  };
+          dur = 0.2;
 
-  xdg.configFile."picom/picom.conf".text =
-    let
-      curve = "cubic-bezier(0.05, 0.9, 0.1, 1.05)";
-      duration = "0.2";
-    in
-    lib.mkAfter ''
-      animations = ({
-        triggers = [ "close", "hide", "open", "show", "geometry" ];
-        scale-x = {
-          curve = "${curve}";
-          duration = ${duration};
-          start = "window-width-before / window-width";
-          end = 1;
-        };
-        scale-y = {
-          curve = "${curve}";
-          duration = ${duration};
-          start = "window-height-before / window-height";
-          end = 1;
-        };
-        offset-x = {
-          curve = "${curve}";
-          duration = ${duration};
-          start = "window-x-before - window-x";
-          end = 0;
-        };
-        offset-y = {
-          curve = "${curve}";
-          duration = ${duration};
-          start = "window-y-before - window-y";
-          end = 0;
-        };
+          scale-x = {
+            curve = "cubic-bezier(0.05, 0.9, 0.1, 1.05)";
+            duration = "dur";
+            start = "window-width-before / window-width";
+            end = 1;
+          };
+          scale-y = {
+            curve = "cubic-bezier(0.05, 0.9, 0.1, 1.05)";
+            duration = "dur";
+            start = "window-height-before / window-height";
+            end = 1;
+          };
+          offset-x = {
+            curve = "cubic-bezier(0.05, 0.9, 0.1, 1.05)";
+            duration = "dur";
+            start = "window-x-before - window-x";
+            end = 0;
+          };
+          offset-y = {
+            curve = "cubic-bezier(0.05, 0.9, 0.1, 1.05)";
+            duration = "dur";
+            start = "window-y-before - window-y";
+            end = 0;
+          };
 
-        shadow-scale-x = "scale-x";
-        shadow-scale-y = "scale-y";
-        shadow-offset-x = "offset-x";
-        shadow-offset-y = "offset-y";
-      })
-    '';
+          shadow-scale-x = "scale-x";
+          shadow-scale-y = "scale-y";
+          shadow-offset-x = "offset-x";
+          shadow-offset-y = "offset-y";
+        })
+      }
+    )
+  '';
 }
