184ee40e7c5313bdc0855ade0c14aeed9a58281e
mimvoid <mimvoid@proton.me>
2025-08-20 19:43:06 -0400

    update Hyprland window rules

--- home-manager/wayland/hypr/windowrules.nix
+++ home-manager/wayland/hypr/windowrules.nix
@@ -1,71 +1,70 @@
 { terminal, ... }:
-
+let
+  setRules = params: rules: builtins.map (rule: "${rule}, ${params}") rules;
+in
 {
-  layerrule = [
-    "animation popin 65%, launcher"
-    "ignorezero, launcher"
-    "blur, launcher"
-    "xray 0, launcher"
-    "dimaround, launcher"
+  layerrule = setRules "launcher" [
+    "animation popin 65%"
+    "ignorezero"
+    "blur"
+    "xray 0"
+    "dimaround"
   ];
 
   windowrulev2 =
     let
-      # Opacity
-      more = { id ? "class" }: name: "opacity 0.9 override 0.75 override, ${id}:(${name})";
-      high = name: "opacity 0.97 override 0.85 override, class:(${name})";
-      active = name: "opacity 1.0 override 0.85 override, class:(${name})";
-      full = name: "opaque, class:(${name})";
+      mapClasses = rule: classes: builtins.map (class: "${rule}, class:${class}") classes;
     in
     [
-      (more { id = "title"; } "*Nextcloud")
-      (more { } "Anki")
-      (more { } "org.pwmt.zathura")
-      (more { } "com.github.flxzt.rnote")
-
-      (high "obsidian")
-      (high "vesktop")
-      (high "ristretto")
-
-      (active "firefox")
-      (active "zen")
-      (active "FFPWA*")
-      (active "libreoffice*")
-      (active "FreeTube")
-      (active "Godot")
-      (active "org.godotengine.Editor")
-
-      (full "krita")
-      (full "org.inkscape.Inkscape")
-      (full "org.kde.digikam")
-      (full "virt-manager")
-
-      "suppressevent maximize, class:.*"
+      "float, class:.*astal"
+      "workspace 9, class:tauonmb"
       "opacity 0.8 override 0.7 override, class:(${terminal})"
-
-      "float, class:(io.Astal.astal)"
-
-      # For some Krita plugin windows
-      "xray 0, class:^krita$, floating:1"
-      "noinitialfocus, class:^krita$, floating:1"
-      "noblur 1, class:^krita$, floating:1"
-      "noborder 1, class:^krita$, floating:1"
-      "noshadow 1, class:^krita$, floating:1"
-
-      # Shimejis
-      "xray 0, class:^Shijima-Qt$, floating:1"
-      "noinitialfocus, class:^Shijima-Qt$, floating:1"
-      "noblur 1, class:^Shijima-Qt$, floating:1"
-      "noborder 1, class:^Shijima-Qt$, floating:1"
-      "noshadow 1, class:^Shijima-Qt$, floating:1"
-      "opaque 1, class:^Shijima-Qt$, floating:1"
-      "decorate 0, class:^Shijima-Qt$, floating:1"
-      "norounding 1, class:^Shijima-Qt$, floating:1"
-
-      # Make Zotero plugin notifications less intrusive
-      "float, class:^(Zotero)$, title:^(Progress)$"
-      "noinitialfocus, class:^(Zotero)$, title:^(Progress)$"
-      "move 100% 100%, class:^(Zotero)$, title:^(Progress)$"
-      "maxsize 75 50, class:^(Zotero)$, title:^(Progress)$"
-    ];
+    ]
+    # Increase opacity somewhat
+    ++ (mapClasses "opacity 0.9 override 0.75 override" [
+      "Anki"
+      ".*zathura"
+      ".*rnote"
+    ])
+    # High opacity
+    ++ (mapClasses "opacity 0.97 override 0.85 override" [
+      "obsidian"
+      "vesktop"
+      "ristretto"
+      "libreoffice.*"
+    ])
+    # Opaque when focused
+    ++ (mapClasses "opacity 1.0 override 0.85 override" [
+      "firefox"
+      "zen"
+      "FFPWA.*"
+      "FreeTube"
+      "Godot"
+      "org.godotengine.Editor"
+    ])
+    ++ (mapClasses "opaque" [
+      "krita"
+      ".*Inkscape"
+      ".*digikam"
+      "virt-manager"
+    ])
+    # For some Krita plugin windows
+    ++ (setRules "class:krita, floating:1" [
+      "xray 0"
+      "noinitialfocus"
+      "noblur"
+      "noborder"
+      "noshadow"
+    ])
+    # Shimejis
+    ++ (setRules "class:Shijima-Qt, floating:1" [
+      "xray 0"
+      "noinitialfocus"
+      "noblur"
+      "noborder"
+      "noshadow"
+      "opaque"
+      "decorate 0"
+      "norounding"
+    ]);
 }
