0192f3720144541a552e31a9989a73f427f447bd
mimvoid <mimvoid@proton.me>
2026-07-30 12:46:37 -0400

    update yazi config

--- home-manager/cli/yazi/plugins.nix
+++ home-manager/cli/yazi/plugins.nix
@@ -4,19 +4,11 @@
   programs.yazi = {
     settings.plugin = {
       prepend_previewers = [ { mime = "audio/*"; run = "exifaudio"; } ];
-      prepend_fetchers = [
-        { id = "git"; name = "*"; run = "git"; }
-        { id = "git"; name = "*/"; run = "git"; }
-      ];
     };
 
     keymap.mgr.prepend_keymap = [
       # Smart paste plugin
       { on = "p"; run = "plugin smart-paste"; }
-
-      # File navigation wraparound plugin
-      { on = "k"; run = "plugin arrow -1"; }
-      { on = "j"; run = "plugin arrow 1"; }
 
       # Max preview
       { on = "T"; run = "plugin toggle-pane max-preview"; }
@@ -47,25 +39,12 @@
         git full-border smart-paste toggle-pane;
     in
     pkgs.voids.lib.prependAttrs "yazi/plugins/" {
-    "bookmarks.yazi".source = pkgs.voids.yaziPlugins.bookmarks;
-    "exifaudio.yazi".source = pkgs.voids.yaziPlugins.exifaudio;
+      "bookmarks.yazi".source = pkgs.voids.yaziPlugins.bookmarks;
+      "exifaudio.yazi".source = pkgs.voids.yaziPlugins.exifaudio;
 
-    "git.yazi".source = git;
-    "full-border.yazi".source = full-border;
-    "smart-paste.yazi".source = smart-paste;
-    "toggle-pane.yazi".source = toggle-pane;
-
-    # Arrow: file navigation wraparound
-    "arrow.yazi/main.lua".text = # lua
-      ''
-        --- @sync entry
-        return {
-          entry = function(_, job)
-            local current = cx.active.current
-            local new = (current.cursor + job.args[1]) % #current.files
-            ya.mgr_emit("arrow", { new - current.cursor })
-          end,
-        }
-      '';
-  };
+      "git.yazi".source = git;
+      "full-border.yazi".source = full-border;
+      "smart-paste.yazi".source = smart-paste;
+      "toggle-pane.yazi".source = toggle-pane;
+    };
 }
