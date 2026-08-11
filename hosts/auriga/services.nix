fd85ed5210f6d6e6f34519890ac5258e7fa345ea
mimvoid <mimvoid@proton.me>
2026-08-11 00:33:14 -0400

    setup reverse proxy and DNS resolver

--- hosts/auriga/services.nix
+++ hosts/auriga/services.nix
@@ -1,4 +1,39 @@
+{ pkgs, ... }:
+let
+  ports = {
+    karakeep = 3000;
+  };
+in
 {
+  services.nginx = {
+    enable = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+
+    appendHttpConfig = ''
+      # Minimize information leaked to other domains
+      add_header 'Referrer-Policy' 'origin-when-cross-origin';
+
+      # Disable embedding as a frame
+      add_header X-Frame-Options DENY;
+
+      # Prevent injection of code in other mime types (XSS attacks)
+      add_header X-Content-Type-Options nosniff;
+    '';
+
+    virtualHosts = {
+      "karakeep.auriga.cafe" = {
+        useACMEHost = "auriga.cafe";
+        forceSSL = true;
+        locations."/".proxyPass = "http://127.0.0.1:${toString ports.karakeep}";
+      };
+    };
+  };
+
+  # Applications
+
   services.karakeep = {
     enable = true;
     package = pkgs.unstable.karakeep; # The stable branch had an insecure pnpm
@@ -10,7 +45,30 @@
     };
   };
 
-  networking.firewall.allowedTCPPorts = [
-    3000 # Karakeep
-  ];
+  # Security & DNS
+
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+  services.pihole-ftl = {
+    enable = true;
+    openFirewallDNS = true;
+    settings.dns = {
+      upstreams = [ "1.1.1.1" "9.9.9.9" ];
+      hosts = [ "10.0.0.27 auriga.cafe karakeep.auriga.cafe" ];
+    };
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    certs."auriga.cafe" = {
+      domain = "auriga.cafe";
+      extraDomainNames = [ "karakeep.auriga.cafe" ];
+      reloadServices = [ "nginx" ];
+
+      dnsProvider = "desec";
+      dnsPropagationCheck = true;
+      credentialFiles.DESEC_TOKEN_FILE = "/var/lib/desec/desec-token";
+    };
+  };
+  users.users.nginx.extraGroups = [ "acme" ];
 }
