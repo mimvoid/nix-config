64117b1413370bb656a5b0e2d2409398575558a8
mimvoid <mimvoid@proton.me>
2026-08-22 11:02:16 -0400

    add all IP addresses for CloudFlare and Quad9 DNS to Pi-Hole

--- modules/nixos/server/pihole.nix
+++ modules/nixos/server/pihole.nix
@@ -12,9 +12,19 @@ in
     openFirewallDNS = true;
     settings.dns = {
       upstreams = [
+        # CloudFlare
         "1.1.1.1"
+        "1.0.0.1"
+        "2606:4700:4700::1111"
+        "2606:4700:4700::1001"
+
+        # Quad9
         "9.9.9.9"
+        "149.112.112.112"
+        "2620:fe::fe"
+        "2620:fe::9"
       ];
+
       hosts = [ "10.0.0.27 ${builtins.concatStringsSep " " auriga-domains}" ];
 
       # HACK: Have Pi-Hole allow requests from Wireguard. This is fine since the router does
